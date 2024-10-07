780739d7f0180e7df6abc1b785f1f191a6e00c57
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2024-10-06 23:17:36 -0300

    feat: improve HomePage
    add new elements for home page

--- backstage-demo-v1/packages/app/src/components/home/HomePage.tsx
+++ backstage-demo-v1/packages/app/src/components/home/HomePage.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Container, Typography, Button, Box } from '@mui/material';
 
-export const HomePage = () => (
-  <h1>Welcome to Backstage!</h1>
-);
+export const HomePage = () => {
+  const handleButtonClick = () => {
+    alert("Exploring Backstage!");
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Box 
+        display="flex" 
+        flexDirection="column" 
+        justifyContent="center" 
+        alignItems="center" 
+        height="100vh"
+      >
+        <Typography variant="h2" gutterBottom>
+          Welcome to Backstage!
+        </Typography>
+        <Typography variant="h6" color="textSecondary" paragraph>
+          Your platform for building developer portals.
+        </Typography>
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={handleButtonClick}
+        >
+          Start Exploring
+        </Button>
+      </Box>
+    </Container>
+  );
+};
